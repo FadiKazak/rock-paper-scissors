3fd80617f6a81091a3ec61be55a66495f2a73d43
Fadi <72884667+FadiKazak@users.noreply.github.com>
2023-02-21 20:50:45 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 # Rock Paper Scissors:
 
 import random
-
-# import string # Not being used at the moment.
-# import os # Not being used at the moment. 
+import string
+import os
 
 print("Welcome to the rock paper scissors game, good luck!") 
 
@@ -18,28 +17,48 @@ while True:
   
   print("the computer chose:", computer_input)
   
-  if user_inputs == "r" or user_inputs == "R" and computer_input == "rock":
+  if user_inputs == "r" and computer_input == "rock":
     print("It is a tie!") 
-  elif user_inputs == "r" or user_inputs == "R" and computer_input == "paper":
+  elif user_inputs == "r" and computer_input == "paper":
     print("The computer wins!")
-  elif user_inputs == "r" or user_inputs == "R" and computer_input == "scissors":
+  elif user_inputs == "r" and computer_input == "scissors":
     print("You won!")
-  elif user_inputs == "p" or user_inputs == "P" and computer_input == "rock":
+  elif user_inputs == "p" and computer_input == "rock":
     print("You win!") 
-  elif user_inputs == "p" or user_inputs == "P" and computer_input == "paper":
+  elif user_inputs == "p" and computer_input == "paper":
     print("It is a tie!")
-  elif user_inputs == "p" or user_inputs == "P" and computer_input == "scissors":
+  elif user_inputs == "p" and computer_input == "scissors":
     print("The computer wins!") 
-  elif user_inputs == "s" or user_inputs == "S" and computer_input == "rock":
+  elif user_inputs == "s" and computer_input == "rock":
     print("The computer wins!")
-  elif user_inputs == "s" or user_inputs == "S" and computer_input == "paper":
+  elif user_inputs == "s" and computer_input == "paper":
     print("You win!")
-  elif user_inputs == "s" or user_inputs == "S" and computer_input == "scissors":
+  elif user_inputs == "s" and computer_input == "scissors":
+    print("It is a tie!")
+  elif user_inputs == "R" and computer_input == "rock":
+    print("It is a tie!") 
+  elif user_inputs == "R" and computer_input == "paper":
+    print("The computer wins!")
+  elif user_inputs == "R" and computer_input == "scissors":
+    print("You won!")
+  elif user_inputs == "P" and computer_input == "rock":
+    print("You win!") 
+  elif user_inputs == "P" and computer_input == "paper":
+    print("It is a tie!")
+  elif user_inputs == "P" and computer_input == "scissors":
+    print("The computer wins!") 
+  elif user_inputs == "S" and computer_input == "rock":
+    print("The computer wins!")
+  elif user_inputs == "S" and computer_input == "paper":
+    print("You win!")
+  elif user_inputs == "S" and computer_input == "scissors":
     print("It is a tie!")
 
   go_again = input("Do you want to play again (Y/N)? ") 
 
   if go_again == "n" or go_again == "N":
+    print("Okay, see you next time!")
     break 
     
   # os.system('cls') # Not working?
+  
